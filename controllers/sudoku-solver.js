5b890a6a0826f6fd5c7bde98835bfda793b22af9
Wes Sitzes <polybiuspro@protonmail.com>
2025-02-22 12:29:32 -0500

    feat: create solver validate function

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,24 +1,27 @@
 class SudokuSolver {
+    validate(puzzleString) {
+        const PUZZLE_REGEX = /^[1-9\.]+$/g;
 
-  validate(puzzleString) {
-  }
+        if (puzzleString.length !== 81) {
+            return {
+                error: "Expected puzzle to be 81 characters long",
+            };
+        }
 
-  checkRowPlacement(puzzleString, row, column, value) {
+        if (!PUZZLE_REGEX.test(puzzleString)) {
+            return { error: "Invalid characters in puzzle" };
+        }
 
-  }
+        return { valid: true };
+    }
 
-  checkColPlacement(puzzleString, row, column, value) {
+    checkRowPlacement(puzzleString, row, column, value) {}
 
-  }
+    checkColPlacement(puzzleString, row, column, value) {}
 
-  checkRegionPlacement(puzzleString, row, column, value) {
+    checkRegionPlacement(puzzleString, row, column, value) {}
 
-  }
-
-  solve(puzzleString) {
-    
-  }
+    solve(puzzleString) {}
 }
 
 module.exports = SudokuSolver;
-
