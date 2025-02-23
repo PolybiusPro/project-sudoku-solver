ee7227f1d7f4ec931739c48223e8c9174ec7909c
Wes Sitzes <polybiuspro@protonmail.com>
2025-02-23 12:14:11 -0500

    feat: create row map

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -37,4 +37,16 @@ const createMatrix = (puzzleString) => {
     return matrix;
 };
 
+const rowMap = {
+    A: 0,
+    B: 1,
+    C: 2,
+    D: 3,
+    E: 4,
+    F: 5,
+    G: 6,
+    H: 7,
+    I: 8,
+};
+
 module.exports = SudokuSolver;
